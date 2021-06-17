758127b1eb0bace3ed60a6aaeb4553e48ce0aff9
_tailwind <46327788+tailwind01@users.noreply.github.com>
2021-06-17 10:54:29 +0530

    Create cf1409a-two ints.cpp

---
+++ cf1409a-two ints.cpp
@@ -0,0 +1,46 @@
+//publisher tailwind
+
+#include <iostream>
+using namespace std;
+int diffs(long long int a, long long int b){
+    
+    long long int ans = 0;
+    long long int absDiff = 0;
+    
+    if(a>b){
+        absDiff = a-b;
+        if(absDiff % 10 != 0 && absDiff>10){
+            ans = absDiff/10 + 1;
+        }
+        else if(absDiff % 10 == 0 && absDiff>10){
+            ans = absDiff/10;
+        }
+        else{
+            ans = 1;
+        }
+    }
+    else if(b>a){
+        absDiff = b - a;
+        if(absDiff % 10 != 0 && absDiff>10){
+            ans = absDiff/10 + 1;
+        }
+        else if(absDiff % 10 == 0 && absDiff>10){
+            ans = absDiff/10;
+        }
+        else{
+            ans = 1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int nc;
+    cin >> nc;
+    for(int j=0;j<nc;j++){
+        long long int a,b;
+        cin >> a >> b;
+        cout << diffs(a,b) << endl;
+    }
+    return 0;
+}
