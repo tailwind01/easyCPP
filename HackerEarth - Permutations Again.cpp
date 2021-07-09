2a937bef51254d1f3e841cf6b9614a3bc038e6c2
_tailwind <46327788+tailwind01@users.noreply.github.com>
2021-07-10 02:00:33 +0530

    Create HackerEarth - Permutations Again.cpp

---
+++ HackerEarth - Permutations Again.cpp
@@ -0,0 +1,31 @@
+//signed tailwind, 0.25 sec execution, less than 2Mbs memory
+
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+   int numcases;
+   cin >> numcases;
+   int maxnum = 100001;
+	vector <unsigned long long int> allnums={0,1,1};
+    
+    for(int i=3; i<maxnum;i++){
+        if(i%2==0){
+            unsigned long long int mplier = i/2;
+            allnums.push_back(mplier*i - 1);
+        }
+        else{
+            unsigned long long int mplier = i/2;
+            unsigned long long int addn = i/2 -1 ;
+            allnums.push_back(mplier*i + addn);
+        }
+    }
+   
+   for(int j=0; j<numcases;j++){
+       int givennum;
+       cin >> givennum;
+       cout << allnums[givennum] << endl;
+   }
+   return 0;
+}
